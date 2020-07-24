6a6cc18cc4896e5b1e5b0d7a37a85750c033a247
hhm19990929 <65931506+hhm19990929@users.noreply.github.com>
2020-07-24 11:04:11 +0800

    ad Class\Style

--- toutiao_weixin/client/onekit/ui/ad/ad.js
+++ toutiao_weixin/client/onekit/ui/ad/ad.js
@@ -1,9 +1,15 @@
 Component({
-  mixins: [],
+  options: {
+    styleIsolation: 'apply-shared'
+  },
   data: {},
-  props: {},
-  didMount() {},
-  didUpdate() {},
-  didUnmount() {},
-  methods: {},
+  properties: { 
+    Class:{type:String,value:""},
+    Style:{type:String,value:""},
+    unitId:{type:String,value:""},
+    adIntervals:{type:Number,value:0},
+  },
+  methods:{
+
+  }
 });
