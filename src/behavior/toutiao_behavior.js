7f6d6321a23a8092f1e95415c0c543d08b5c41a1
gongyuan2 <939014677@qq.com>
2020-09-24 16:01:15 +0800

    v.01

--- src/behavior/toutiao_behavior.js
+++ src/behavior/toutiao_behavior.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable camelcase */
 module.exports = Behavior({
 
@@ -5,17 +6,53 @@ module.exports = Behavior({
 
   },
   methods: {
-    ui_tap(tt_e) { this.triggerEvent('tap', tt_e) },
-    ui_touchstart(tt_e) { this.triggerEvent('touchstart', tt_e) },
-    ui_touchmove(tt_e) { this.triggerEvent('touchmove', tt_e) },
-    ui_touchcancel(tt_e) { this.triggerEvent('touchcancel', tt_e) },
-    ui_touchend(tt_e) { this.triggerEvent('touchend', tt_e) },
-    ui_longpress(tt_e) { this.triggerEvent('longpress', tt_e) },
-    ui_longtap(tt_e) { this.triggerEvent('longtap', tt_e) },
-    ui_transitionend(tt_e) { this.triggerEvent('transitionend', tt_e) },
-    ui_animationstart(tt_e) { this.triggerEvent('animationstart', tt_e) },
-    ui_animationiteration(tt_e) { this.triggerEvent('animationiteration', tt_e) },
-    ui_animationend(tt_e) { this.triggerEvent('animationend', tt_e) },
-    ui_touchforcechange(tt_e) { this.triggerEvent('touchforcechange', tt_e) },
+    ui_tap(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('tap', tt_e)
+    },
+    ui_touchstart(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('touchstart', tt_e)
+    },
+    ui_touchmove(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('touchmove', tt_e)
+    },
+    ui_touchcancel(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('touchcancel', tt_e)
+    },
+    ui_touchend(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('touchend', tt_e)
+    },
+    ui_longpress(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('longpress', tt_e)
+    },
+    ui_longtap(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('longtap', tt_e)
+    },
+    ui_transitionend(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('transitionend', tt_e)
+    },
+    ui_animationstart(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('animationstart', tt_e)
+    },
+    ui_animationiteration(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('animationiteration', tt_e)
+    },
+    ui_animationend(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('animationend', tt_e)
+    },
+    ui_touchforcechange(tt_e) {
+      console.log('ui_tap')
+      this.triggerEvent('touchforcechange', tt_e)
+    },
   },
 })
