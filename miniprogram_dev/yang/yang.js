eec1c71e3a903197425d47809dc84759aab6dcef
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2020-09-27 18:02:55 +0800

    v.09

--- miniprogram_dev/yang/yang.js
+++ miniprogram_dev/yang/yang.js
@@ -1,19 +1,17 @@
 // yang/yang.js
 Page({
-
+  switch1Change: function (e){
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  },
+  switch2Change: function (e){
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  },
   /**
    * 页面的初始数据
    */
   data: {
-    icons: [
-      ['success', 'success_no_circle'],
-      ['info', 'warn'],
-      ['waiting', 'clear'],
-      ['cancel', 'download'],
-      ['search']
-  ],
-  sizes: [66, 48, 36, 24],
-  colors: ['#222222', '#CACACA', '', '#50ABF9']
+    
+  
   },
 
   /**
