19166c2126cb8da5725d50b2214876294610f969
lyx0lwz <69792963+lyx0lwz@users.noreply.github.com>
2020-09-29 15:01:57 +0800

    .

--- miniprogram_dev/li/li.js
+++ miniprogram_dev/li/li.js
@@ -1,11 +1,14 @@
 // li/li.js
-Page({
+import OnekitPage from "../toutiao2weixin/OnekitPage"
+OnekitPage({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // icons: [['success', 'success_no_circle'], ['info', 'warn'], ['waiting', 'clear'], ['cancel', 'download'], ['search']],
+    // sizes: [66, 48, 36, 24],
+    // colors: ['#222222', '#CACACA', '', '#50ABF9']
   },
 
   /**
