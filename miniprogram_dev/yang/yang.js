8197677a898b85f4a2fd29a4d7f6d1b28ee1689d
lyx0lwz <69792963+lyx0lwz@users.noreply.github.com>
2020-09-30 15:02:51 +0800

    。

--- miniprogram_dev/yang/yang.js
+++ miniprogram_dev/yang/yang.js
@@ -1,38 +1,18 @@
-import OnekitPage from '../toutiao2weixin/OnekitPage';
-
-OnekitPage({
-    data:{
-        icons:[
-            [
-                'success',
-                'success_no_circle'
-            ],
-            [
-                'info',
-                'warn'
-            ],
-            [
-                'waiting',
-                'clear'
-            ],
-            [
-                'cancel',
-                'download'
-            ],
-            [
-                'search'
-            ]
-        ],
-        sizes:[
-            68,
-            50,
-            44
-        ],
-        colors:[
-            '#222222',
-            '#CACACA',
-            '',
-            '#50ABF9'
-        ]
+const pageData = {
+  onShareAppMessage() {
+    return {
+      title: 'slider',
+      path: 'page/component/pages/slider/slider'
     }
-});
+  },
+}
+
+for (let i = 1; i < 5; ++i) {
+  (function (index) {
+    pageData['slider' + index + 'change'] = function (e) {
+      console.log('slider' + index + '发生change事件，携带值为', e.detail.value)
+    }
+  }(i))
+}
+
+Page(pageData)
