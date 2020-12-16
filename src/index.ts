4a6f384b2b05b800f04d1a0ecfedfcc20eb38135
lyx0lwz <1850115720@qq.com>
2020-12-16 11:46:41 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,11 @@ import OnekitBehavior from './OnekitBehavior'
 import OnekitComponent from './OnekitComponent'
 import OnekitPage from './OnekitPage'
 import tt from './tt'
-import OneKit from './js/OneKit'
 
 export {
   OnekitApp,
   OnekitBehavior,
   OnekitComponent,
   OnekitPage,
-  tt,
-  OneKit
+  tt
 }
